4960619ea99ea055616bf37ed70ae1a467923673
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-10-09 19:15:55 -0400

    added the required path files

---
+++ index.js
@@ -0,0 +1,6 @@
+const { prompt } = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
