8e791b0935ed3245205cd0845b77463a36076715
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-10-09 19:26:43 -0400

    added the starting code for asking the questions and writing the html file

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
+
+const askQuestions = (fileName, data) => {
+    prompt([
+        {
+
+        }
+    ])
+    .then((answers) => {
+        const {} = answers;
+
+        fs.writeFile(fileName, html, (err) =>
+        err ? console.error(err) : console.log("Successfully created an HTML File!")
+        );
+    });
+};
