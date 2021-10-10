fee9f0cd9c0906d7d0493b42c03f4c28c3d365d6
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-10-09 20:02:40 -0400

    added the questions that will be asked

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
+const questions = [
+    "What is the team manager's name?",
+    "What is the team manager's id?",
+    "What is the team manager's email?",
+    "What is the team manager's office number?",
+    "Which type of team member do you want to add?",
+    "What is your engineer's name?",
+    "What is your engineer's id?",
+    "What is your engineer's email?",
+    "What is your engineer's GitHub username?",
+    "What is your intern's name?",
+    "What is your intern's id?",
+    "What is your intern's email?",
+    "What is your intern's school?"
+]
+
 const askQuestions = (fileName, data) => {
     prompt([
         {
@@ -19,3 +35,10 @@ const askQuestions = (fileName, data) => {
         );
     });
 };
+
+function init() {
+    console.log("Lets build your team!");
+    askQuestions("./dist/team.js", questions);
+};
+
+init();
